94a5017e9c96e7add5cd0a70d2667b58e47e83b5
Isabella de Leon <ijdeleon@berkeley.edu>
2020-05-28 17:52:46 -0700

    Added Update Metrics to ArticleService and dashbord Utilities

--- ArticleJavaServer/demo/src/main/java/com/example/demo/service/ArticleService.java
+++ ArticleJavaServer/demo/src/main/java/com/example/demo/service/ArticleService.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.TimeUnit;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -84,35 +85,63 @@ public class ArticleService {
 		List<ArticleEntity> buzzArticles = articleRepository.findByStatusCode("BUZZ");
 		List<ArticleEntity> userArticles = articleRepository.findByStatusCode("USER");
 		for (ArticleEntity article : buzzArticles) {
-			String title = article.getTitle();
+			logger.info("ITERATING THROUGH BUZZ ARTICLES:");
+			String title = article.getArticleTitle();
 			if (title.equals("")) {
+				logger.info("TITLE = \"\"");
 				continue;
 			}
-			logger.info(title);
 			String url = article.getUrl();
-			JSONObject jArticle = buzzService.getBuzz(url);
-			logger.info(jArticle.toString());
+			JSONObject jArticle = new JSONObject();
+
+			try {
+				jArticle = buzzService.getBuzz(url);
+			} catch(Exception e) {
+				logger.info("BuzzService Error");
+				continue;
+			}
+
+			if (jArticle == null) {
+				logger.info("jArticle is NULL");
+				continue;
+			}
+
 			if (jArticle.get("author_name").toString().equals("none")) {
 				continue;
 			}
+
 			ArticleEntity updatedArticle = updateArticleWithBuzz(jArticle, article);
+			logger.info("UPDATING BUZZ ARTICLE:");
 			Integer updatedAt = Integer.parseInt(new SimpleDateFormat("YYYYMMDD").format(new Date()));
 			updatedArticle.setUpdatedAt(updatedAt);
 			articleRepository.save(updatedArticle);
 			list.put(jArticle);
+			try {
+				TimeUnit.SECONDS.sleep(1);
+			} catch(InterruptedException e) {
+				logger.info("TimeUnit Error");
+			}
+			
 		}
 
 		for (ArticleEntity article : userArticles) {
+			logger.info("ITERATING THROUGH USER ARTICLES:");
 			String url = article.getUrl();
 			JSONObject jArticle = buzzService.getBuzz(url);
 			if (jArticle.get("author_name").toString().equals("none")) {
 				continue;
 			}
-			ArticleEntity updatedArticle = updateArticleWithBuzz(jArticle, article);
+			logger.info("UPDATING USER ARTICLE:");
+			ArticleEntity updatedArticle = updateArticleWithBuzz(jArticle, article);			
 			Integer updatedAt = Integer.parseInt(new SimpleDateFormat("YYYYMMDD").format(new Date()));
 			updatedArticle.setUpdatedAt(updatedAt);
 			articleRepository.save(updatedArticle);
 			list.put(jArticle);
+			try {
+				TimeUnit.SECONDS.sleep(1);
+			} catch(InterruptedException e) {
+				logger.info("TimeUnit Error");
+			}
 		}
 
 		return list;
--- peclient/src/app/utilities/utilities.service.ts
+++ peclient/src/app/utilities/utilities.service.ts
@@ -28,5 +28,9 @@ export class UtilitiesService {
   doBuzz() {
   	return this.http.get(this.apiDest + '/article/buzz2');
   }
+
+  doMetrics() {
+    return this.http.get(this.apiDest + '/article/update')
+  }
 }
  
